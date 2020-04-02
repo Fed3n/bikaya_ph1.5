a165213013596082cbfdfe2bcc96441d482cb633
Fed3n <fdemarchi95@gmail.com>
2020-04-02 18:34:56 +0200

    corretta init, carica ed esegue processo su entrambi

--- main.c
+++ main.c
@@ -21,18 +21,22 @@
 #define RAM_FRAMESIZE 4096
 #define WORDSIZE 4
 
-static termreg_t *term0_reg = (termreg_t *)DEV_REG_ADDR(IL_TERMINAL, 0);
-#define CMD_ACK 1
-
 #ifdef TARGET_UMPS
 void termprint(char *str);
 
 /*interrupt disabled, kernel mode, local timer on, virtual memory off*/
 #define STATUS_ALL_INT_DISABLE_KM_LT(status) ((status) | (STATUS_TE))
-#define STATUS_ALL_INT_ENABLE_KM_LT(status) ((status) | (STATUS_IEc) | (STATUS_IM_MASK) | (STATUS_TE))
+#define STATUS_ALL_INT_ENABLE_KM_LT(status) ((status) | (STATUS_IEc) | STATUS_IM(1) | (STATUS_TE))
 
+/*macro da passare a LDST*/
 #define TO_LOAD(status) (status)
+/*macro per accedere al pc da state_t*/
+#define ST_PC pc_epc
 
+/*word count da modificare prima di chiamare LDST dopo un'eccezione*/
+#define SYSBP_PC 1
+
+/*inizializza l'exception area in kernel mode, interrupt disabilitati*/
 void init_excarea(state_t* p, void* handler){
 	ownmemset(p, 0, sizeof(state_t));
 	p->reg_sp = RAMTOP;
@@ -42,13 +46,14 @@ void init_excarea(state_t* p, void* handler){
 	p->status = STATUS_ALL_INT_DISABLE_KM_LT(p->status);
 }
 
-/*inizializza processo in kernel mode, interrupt abilitati*/
+/*inizializza processo in kernel mode, interrupt disabilitati escluso timer*/
+/*n = numero del processo, che influenza dove gli viene assegnato spazio in ram*/
 void initProcess_KM(state_t* p, void* fun, int n){
 	ownmemset(p, 0, sizeof(state_t));
 	p->reg_sp = (RAMTOP-(RAM_FRAMESIZE*n));
 	/*Inizializzo sia pc_epc che reg_t9 all'entry point come dal manuale di umps*/
 	p->pc_epc = (memaddr)fun;
-	p->reg_t9 = (memaddr)fun;	
+	p->reg_t9 = (memaddr)fun;
 	p->status = STATUS_ALL_INT_ENABLE_KM_LT(p->status);
 }
 
@@ -57,8 +62,15 @@ void initProcess_KM(state_t* p, void* fun, int n){
 #ifdef TARGET_UARM
 #define termprint(str) tprint(str);
 
+/*macro per usare LDST su uarm*/
 #define TO_LOAD(status) &(status->a1)
+/*macro per accedere al pc da state_t*/
+#define ST_PC pc
 
+/*word count da modificare prima di chiamare LDST dopo un'eccezione*/
+#define SYSBP_PC 0
+
+/*inizializza l'exception area in kernel mode, interrupt disabilitati*/
 void init_excarea(state_t* p, void* handler){
 	ownmemset(p, 0, sizeof(state_t));
 	p->pc = (memaddr)handler;
@@ -68,13 +80,15 @@ void init_excarea(state_t* p, void* handler){
 	p->CP15_Control = CP15_DISABLE_VM(p->CP15_Control);
 }
 
-/*inizializza processo in kernel mode, interrupt abilitati*/
+/*inizializza processo in kernel mode, interrupt disabilitati escluso timer*/
+/*n = numero del processo, che influenza dove gli viene assegnato spazio in ram*/
 void initProcess_KM(state_t* p, void* fun, int n){
 	ownmemset(p, 0, sizeof(state_t));
 	p->pc = (memaddr)fun;
 	p->sp = (RAMTOP-(RAM_FRAMESIZE*n));
 	p->cpsr = (p->cpsr | STATUS_SYS_MODE);
-	p->cpsr = STATUS_ALL_INT_ENABLE(p->cpsr);
+	p->cpsr = STATUS_DISABLE_INT(p->cpsr);
+	p->cpsr = STATUS_ENABLE_TIMER(p->cpsr);
 	p->CP15_Control = CP15_DISABLE_VM(p->CP15_Control);
 }
 
@@ -91,16 +105,15 @@ void testx(){
 
 void test(){
 	termprint("Hi there!\n");
-	termprint("Test!");
+	SYSCALL(0,0,0,0);
+	termprint("Back to test\n");
 	HALT();
 }
 
 void handleINT(){
 	termprint("INTERRUPT!");
-	term0_reg->transm_command = CMD_ACK;
-	state_t* p = (state_t *)INT_OLDAREA;
-	p->pc = p->pc - WORDSIZE;
-	LDST(TO_LOAD(p));
+	/*prima di ridare controllo al processo qua dovremmo diminuire di 1 word il pc a uarm, niente su umps*/
+	HALT();
 }
 
 void handleTLB(){
@@ -115,6 +128,10 @@ void handleTRAP(){
 
 void handleSYSBP(){
 	termprint("SYSBP!");
+	state_t* p = (state_t *)SYSBK_OLDAREA;
+	/*prima di ridare controllo al processo incrementiamo di 1 word il pc a umps, niente su uarm*/
+	p->ST_PC = p->ST_PC + SYSBP_PC*WORDSIZE;
+	LDST(TO_LOAD(p));
 	HALT();
 }
 
@@ -134,18 +151,23 @@ init_excarea((state_t *)SYSBK_NEWAREA, handleSYSBP);
 
 }
 
-/*Quello che fa al momento è un test che rimbalza l'interrupt in una syscall che a sua volta
-torna dove era rimasto l'interrupt e fa HALT()*/
+/*Inizializza le exception area, i PCBs e poi avvia un processo con LDST*/
 int main(){
 	initAreas();
-	//termprint("AREA DONE!\n");
+	termprint("AREA DONE!\n");
+
 	initPcbs();
-	//termprint("PCB DONE!\n");
+	termprint("PCB DONE!\n");
+
 	struct pcb_t* a = allocPcb();
 	initProcess_KM(&a->p_s, test, 1);
-	//termprint("PROCESS INITIALIZED!\n");
+	termprint("PROCESS INITIALIZED!\n");
+
+	/*Qua nella versione finale immagino andrà chiamato schedule() (e forse prima inizializzato il timer)*/
+
 	state_t* p = &(a->p_s);
 	LDST(TO_LOAD(p));
+	/*Se arriva qua sotto dopo LDST qualcosa è andato così storto dall'aver infranto ogni regola dell'emulatore*/
 	termprint("Oh no\n");
 
 	for(;;)
