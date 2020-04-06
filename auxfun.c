0f4c96a48870d000c426c50c9eb9fec4ec67714e
Fed3n <fdemarchi95@gmail.com>
2020-04-06 12:06:10 +0200

    memcpy in auxfun

--- auxfun.c
+++ auxfun.c
@@ -7,3 +7,10 @@ void ownmemset(void *mem, char val, unsigned int size){
 		((char*)mem)[i] = val;
 	}
 }
+
+void ownmemcpy(void *src, void *dest, unsigned int size){
+	int i;
+	for(i = 0; i < size; i++){
+		((char*)dest)[i] = ((char*)src)[i];
+	}
+}
